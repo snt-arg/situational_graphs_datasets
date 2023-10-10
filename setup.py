8b77453ddcb9bde8afce5638add0f480c8e3446c
JoseAndresMR <josmilrom@gmail.com>
2023-10-10 18:09:07 +0200

    [feat] - as python package

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'graph_datasets'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'),glob(os.path.join('config', '*.json')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='TODO',
+    maintainer_email='josmilrom@gmail.com',
+    license='TODO: License declaration',
+    tests_require=['pytest']
+)
