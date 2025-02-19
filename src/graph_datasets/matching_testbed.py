e7280f617f8ec5e970585ee92a1bf85162a56218
Matteo <matteogiorgi196@gmail.com>
2025-02-19 13:19:53 +0100

    test: generated digraph datasets

--- src/graph_datasets/matching_testbed.py
+++ src/graph_datasets/matching_testbed.py
@@ -8,13 +8,13 @@ from graph_datasets.config import get_config as get_datasets_config
 synteticdataset_settings = get_datasets_config("graph_matching")
 dataset_generator = SyntheticDatasetGenerator(synteticdataset_settings, logger = None, report_path = "AS_Datasets", dataset_name = "test")
 dataset_generator.create_dataset()
-a_graphs_list = dataset_generator.graphs["original"]
-s_graphs_list = dataset_generator.graphs["noise"]
-extended_list = dataset_generator.graphs["extended"]
 # filtered_nxdataset = dataset_generator.get_filtered_datset(settings_hdata["nodes"],settings_hdata["edges"])["noise"]
 
 extended_nxdatset = dataset_generator.extend_nxdataset(dataset_generator.graphs["noise"], "training", "training")
 
+a_graphs_list = dataset_generator.graphs["original"]
+s_graphs_list = dataset_generator.graphs["noise"]
+extended_list = dataset_generator.graphs["extended"]
 # normalized_nxdatset = dataset_generator.normalize_features_nxdatset(extended_nxdatset)
 # view1 = dataset_generator.graphs["views"][0].filter_graph_by_node_attributes_containted({"view" : 1})
 # view2 = dataset_generator.graphs["views"][0].filter_graph_by_node_attributes_containted({"view" : 2})
@@ -28,26 +28,30 @@ extended_nxdatset = dataset_generator.extend_nxdataset(dataset_generator.graphs[
 # all_dataset = extended_nxdatset["train"] + extended_nxdatset["test"] +extended_nxdatset["val"]
 
 # serialize dataset
-dataset_generator.serialize_dataset()
+dataset_generator.serialize_dataset(digraphs=True)
 # for i in range(len(s_graphs_list)):
 #     gv.visualize_nxgraph(a_graphs_list[i], "original", visualize_alone=True)
 #     gv.visualize_nxgraph(s_graphs_list[i], "noise", visualize_alone=True)
 #     gv.visualize_nxgraph(dataset_generator.graphs["extended"][i], "extended", visualize_alone=True)
 
 
-# print(len(a_graphs_list))
-# print(len(s_graphs_list))
-# print(len(extended_list))
+print(len(a_graphs_list))
+print(len(s_graphs_list))
+print(len(extended_list))
+gv.visualize_nxgraph_pair(a_graphs_list[0], s_graphs_list[0], "serial", visualize_alone=True)
+
 
 # deserialize dataset
-# dataset_generator.deserialize_dataset()
-# a_graphs_list = dataset_generator.graphs["original"]
-# s_graphs_list = dataset_generator.graphs["noise"]
-# extended_list = dataset_generator.graphs["extended"]
+dataset_generator.deserialize_dataset(digraphs=True)
+a_graphs_list = dataset_generator.graphs["original"]
+s_graphs_list = dataset_generator.graphs["noise"]
+extended_list = dataset_generator.graphs["extended"]
 
-# print(len(a_graphs_list))
-# print(len(s_graphs_list))
-# print(len(extended_list))
+print(len(a_graphs_list))
+print(len(s_graphs_list))
+print(len(extended_list))
+
+gv.visualize_nxgraph_pair(a_graphs_list[0], s_graphs_list[0], "imported", visualize_alone=True, g1digraph=True, g2digraph=True)
 
 # for i in range(len(s_graphs_list)):
 #     gv.visualize_nxgraph_pair(a_graphs_list[i], s_graphs_list[i], f"orginal_noise{i}", visualize_alone=True)
