999541d17cad08ebac440f742f5c8b93ba56568d
Matteo <matteogiorgi196@gmail.com>
2025-04-01 21:17:01 +0200

    feat: implement serialization and deserialization methods for MSD dataset

--- src/graph_datasets/matching_testbed.py
+++ src/graph_datasets/matching_testbed.py
@@ -9,7 +9,7 @@ synteticdataset_settings = get_datasets_config("graph_matching")
 dataset_generator = SyntheticDatasetGenerator(synteticdataset_settings, logger = None, report_path = "AS_Datasets", dataset_name = "test")
 
 #load dataset 
-dataset_generator.deserialize_and_transform_to_GWraph()
+dim = dataset_generator.deserialize_and_transform_to_GWraph()
 # dataset_generator.create_dataset()
 # # filtered_nxdataset = dataset_generator.get_filtered_datset(settings_hdata["nodes"],settings_hdata["edges"])["noise"]
 
@@ -19,11 +19,23 @@ a_graphs_list = dataset_generator.graphs["original"]
 s_graphs_list = dataset_generator.graphs["noise"]
 extended_list = dataset_generator.graphs["extended"]
 
-# print first 5 graphs
-for i in range(5):
+# print first 1 graphs
+for i in range(1):
     print(f"Original Graph {i}:")
-    print(extended_list[i].get_attributes_of_all_nodes())
-    print(extended_list[i].get_attributes_of_all_edges())
+    print("Node Attributes:")
+    for node, attributes in extended_list[i].get_attributes_of_all_nodes():
+        print(f"  Node {node}:\n\t {attributes}")
+    
+    print("Edge Attributes:")
+    for edge_data in extended_list[i].get_attributes_of_all_edges():
+        edge1, edge2, attributes = edge_data[0], edge_data[1], edge_data[2]
+        print(f"  Edge {edge1}__{edge2}:\n\t {attributes}")
+
+dataset_generator.serialize_MSD_dataset(dim)
+
+dim = dataset_generator.deserialize_MSD_dataset()
+
+print(dim)
 
 # # normalized_nxdatset = dataset_generator.normalize_features_nxdatset(extended_nxdatset)
 # # view1 = dataset_generator.graphs["views"][0].filter_graph_by_node_attributes_containted({"view" : 1})
