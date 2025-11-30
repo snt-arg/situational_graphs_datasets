a5981a78dfd0fedacbbe98df1d26ae93b51882fa
JoseAndresMR <josmilrom@gmail.com>
2025-11-30 11:23:34 +0100

    [refact] improving config and factory.py

--- setup.py
+++ setup.py
@@ -4,19 +4,21 @@ from glob import glob
 
 package_name = 'graph_datasets'
 
+config_files = glob(os.path.join('config', '**', '*.json'), recursive=True)
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     package_data={
-        'graph_datasets': ['config/*.json'],
+        'graph_datasets': ['config/**/*.json'],
     },
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'config'),glob(os.path.join('config', '*.json')))
+        (os.path.join('share', package_name, 'config'), config_files),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
